60b65acf5db36393b246bb13bc496f070b206756
saynschuman <saynschuman@gmail.com>
2018-05-29 01:29:42 +0300

    comments first part finished

--- src/Article.js
+++ src/Article.js
@@ -3,18 +3,23 @@ export default class Article extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      IsOpenComment: false
     };
   }
 
   render() {
     const { article } = this.props;
     const { isOpen } = this.state;
+
     return (
       <div>
         <h3>{article.author}</h3>
         <button onClick={this.toggleOpen}>{isOpen ? "close" : "open"}</button>
+        <br />
         {this.getBody()}
+        {this.getButton()}
+        {this.getComments()}
       </div>
     );
   }
@@ -25,9 +30,36 @@ export default class Article extends Component {
     return <section>{article.title}</section>;
   }
 
+  getComments() {
+    if (!this.state.IsOpenComment) return null;
+    const { article } = this.props;
+    const articleComments = article.comments.map(comment => (
+      <li className="comments" key={comment.comment_id}>
+        {comment.body}
+      </li>
+    ));
+    return <section>{articleComments}</section>;
+  }
+
+  getButton() {
+    if (!this.state.isOpen) return null;
+    const { IsOpenComment } = this.state;
+    return (
+      <button onClick={this.toggleOpenComment}>
+        {IsOpenComment ? "Закрыть комментарии" : "Открыть комментарии"}
+      </button>
+    );
+  }
+
   toggleOpen = () => {
     this.setState({
       isOpen: !this.state.isOpen
+    });
+  };
+
+  toggleOpenComment = () => {
+    this.setState({
+      IsOpenComment: !this.state.IsOpenComment
     });
   };
 }
--- src/articles.js
+++ src/articles.js
@@ -3,6 +3,18 @@ export const articles = [
     id: "1",
     title: "How to handle state in React. The missing FAQ.",
     author: "Osmel Mora",
+    comments: [
+      {
+        comment_id: "dd21",
+        author_name: "Zhenya",
+        body: "My printer is on fire"
+      },
+      {
+        author_name: "Masha",
+        comment_id: "dd22",
+        body: "My printer is on fire too!"
+      }
+    ],
     url:
       "https://medium.com/react-ecosystem/how-to-handle-state-in-react-6f2d3cd73a0c"
   },
@@ -10,6 +22,18 @@ export const articles = [
     id: "2",
     title: "You might not need React Router",
     author: "Konstantin Tarkus",
+    comments: [
+      {
+        comment_id: "dd23",
+        author_name: "Petya",
+        body: "My printer is on fire"
+      },
+      {
+        comment_id: "dd24",
+        author_name: "Vasya",
+        body: "My printer is on fire too!"
+      }
+    ],
     url:
       "https://medium.freecodecamp.com/you-might-not-need-react-router-38673620f3d"
   },
@@ -17,6 +41,18 @@ export const articles = [
     id: "3",
     title: "Mixins Considered Harmful",
     author: "Dan Abramov",
+    comments: [
+      {
+        comment_id: "dd23",
+        author_name: "Petya",
+        body: "My printer is on fire"
+      },
+      {
+        comment_id: "dd24",
+        author_name: "Vasya",
+        body: "My printer is on fire too!"
+      }
+    ],
     url:
       "https://facebook.github.io/react/blog/2016/07/13/mixins-considered-harmful.html"
   },
@@ -24,12 +60,36 @@ export const articles = [
     id: "4",
     title: "React Design Principles",
     author: "Dan Abramov",
+    comments: [
+      {
+        comment_id: "dd23",
+        author_name: "Petya",
+        body: "My printer is on fire"
+      },
+      {
+        comment_id: "dd24",
+        author_name: "Vasya",
+        body: "My printer is on fire too!"
+      }
+    ],
     url: "https://facebook.github.io/react/contributing/design-principles.html"
   },
   {
     id: "5",
     title: "Don't use .bind() when passing props",
     author: "Dave Ceddia",
+    comments: [
+      {
+        comment_id: "dd23",
+        author_name: "Petya",
+        body: "My printer is on fire"
+      },
+      {
+        comment_id: "dd24",
+        author_name: "Vasya",
+        body: "My printer is on fire too!"
+      }
+    ],
     url: "https://daveceddia.com/avoid-bind-when-passing-props/"
   }
 ];
