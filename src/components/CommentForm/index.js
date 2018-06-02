2c68a9ebc222483beb15fcedf10a9abd218fe115
saynschuman <saynschuman@gmail.com>
2018-06-02 15:40:31 +0300

    basic store

---
+++ src/components/CommentForm/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./index.css";
+
+export default class CommentForm extends Component {
+  state = {
+    comment: "",
+    textValid: true
+  };
+  render() {
+    return (
+      <div>
+        <input
+          className={`${this.state.textValid}`}
+          type="text"
+          value={this.state.comment}
+          onChange={this.handleCommentChange}
+        />
+        <button>Add Comment</button>
+      </div>
+    );
+  }
+  handleCommentChange = e => {
+    const value = e.target.value;
+    this.setState({
+      comment: value,
+      textValid: value.length > 5
+    });
+  };
+}
